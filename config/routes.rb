4ae0ee0d63b5b5500d46ed38dc98a34f2c9cf035
Yu.Maruyama <taro.kirameki@techacademy.jp>
2020-08-07 08:05:53 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "toppages#index"
+  resources :users, only: [:index,:create,:show]
+  get  "signup", to: "users#new" 
 end
