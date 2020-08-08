0c4426848b5be68d73495dfce25c682187cfe2c6
Yu.Maruyama <taro.kirameki@techacademy.jp>
2020-08-08 13:17:01 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ include SessionsHelper
   end
    def counts(user)
     @count_microposts = user.microposts.count
+     @count_followings = user.followings.count
+    @count_followers = user.followers.count
    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "toppages#index"
-  resources :users, only: [:index,:create,:show]
+  resources :users, only: [:index,:create,:show] do
+  member do
+      get :followings
+      get :followers
+    end
+  end
   get  "signup", to: "users#new" 
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
    resources :microposts, only: [:create, :destroy]
+   resources :relationships, only: [:create, :destroy]
 end
